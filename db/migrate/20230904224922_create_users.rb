08a19017d9971db10e7a2fce74c9b55a5b92fffe
Jesus Manuel Crosthwaite <manuelxwhite@gmail.com>
2023-09-04 16:33:36 -0700

    WIP Basic user registration

--- db/migrate/20230904224922_create_users.rb
+++ db/migrate/20230904224922_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :first_name, null: false
       t.string :last_name, null: false
-      t.string :email, index: { unique: true }, null: false
+      t.string :email, index: { unique: true }, null: false, default: ""
       t.string :username, index: { unique: true }, null: false
       t.string :external_user_id
 
