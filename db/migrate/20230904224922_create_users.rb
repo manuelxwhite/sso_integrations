f979f0bb37f4aef4dd4638de9a6db1cc0a52a2aa
Jesus Manuel Crosthwaite <manuelxwhite@gmail.com>
2023-09-04 15:50:38 -0700

    base user migration and model

---
+++ db/migrate/20230904224922_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, index: { unique: true }, null: false
+      t.string :username, index: { unique: true }, null: false
+      t.string :external_user_id
+
+      t.timestamps
+    end
+  end
+end
